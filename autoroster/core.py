92e6a619a8adaf83a7a43f14422c44686322ed36
Daniel Smith <nalisarc@gmail.com>
2018-06-27 16:02:57 -0600

    added in filtering by column, and started sheet name restrictions

--- autoroster/core.py
+++ autoroster/core.py
@@ -16,8 +16,8 @@ def open_roster(path, skip=2):
     df = pandas.DataFrame(list(data), columns=cols)
 
     return df
-def get_exam_list(dataframe):
-    raw_exam_list = list(dataframe["SP Exam"].unique())
+def get_exam_list(dataframe, column):
+    raw_exam_list = list(dataframe[column].unique())
     exam_list = [exam for exam in raw_exam_list if str(type(exam)) == "<class 'str'>"]
     exam_list.sort()
     return exam_list
--- tests/test_filter_input.py
+++ tests/test_filter_input.py
@@ -7,5 +7,10 @@ def setup():
 
 def test_organizes_by_exam():
     testr = autoroster.core.open_roster("./testrosters/testrosterminimal.xlsx")
-    test_exams = autoroster.core.get_exam_list(testr)
+    test_exams = autoroster.core.get_exam_list(testr, "SP Exam")
     assert any([(x in ['Biol 3040', 'Cs 2050', 'Engl 4050']) for x in test_exams]), test_exams
+
+def test_can_set_column_to_filter():
+    testr = autoroster.core.open_roster("./testrosters/testrosterminimal.xlsx")
+    test_exams = autoroster.core.get_exam_list(testr, "Notes",)
+    assert test_exams == [], test_exams #This field is empty
